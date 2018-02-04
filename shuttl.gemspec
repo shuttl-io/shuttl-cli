c29643ef79fdbe83e9d11a7edb8579f74f0e4c76
Yoseph Radding <yoseph@shuttl.io>
2018-02-03 20:34:01 -0500

    Made shuttl-cli into a ruby gem!

---
+++ shuttl.gemspec
@@ -0,0 +1,18 @@
+require 'rake';
+
+Gem::Specification.new do |s|
+    s.name        = 'shuttl'
+    s.version     = '1.0.0'
+    s.date        = '2010-04-28'
+    s.summary     = "Shuttl!"
+    s.description = "Shuttl builds doccker images easily"
+    s.authors     = ["Yoseph Radding"]
+    s.email       = 'yoseph@shuttl.io'
+    s.files       = FileList["bin/shuttl", 'src/**/*'].to_a
+    s.homepage    =
+      'http://rubygems.org/gems/shuttl'
+    s.license       = 'MIT'
+    s.post_install_message = "Thanks for installing! Run shuttl install to complete the install"
+    s.executables   = ["shuttl"]
+    s.require_paths = ["src"]
+  end
---
+++ src/commands/install.rb
@@ -0,0 +1,38 @@
+require 'docker'
+require 'json'
+require 'colorize'
+require 'net/http'
+require 'zip'
+
+require_relative '../dsl/eval'
+require_relative 'base'
+
+class Install < CommandBase
+
+    def run (options)
+        tempFile = Tempfile.new('shuttl.zip')        
+        Net::HTTP.start("s3.us-east-2.amazonaws.com") do |http|
+            resp = http.get("/shuttl-cli/shuttlinfo.zip")
+            tempFile.write(resp.body)
+        end
+        tempFile.close
+        Zip::File.open(tempFile.path) do |zip_file|
+            # Handle entries one by one
+            zip_file.each do |entry|
+              # Extract to file/directory/symlink
+              entry.extract(File.join(options[:installPath], entry.name.gsub('.shuttl/', ''))) {true}
+            end
+        end
+        tempFile.unlink
+
+        base = {
+            :images => {},
+            :containers => {},
+        }
+
+        File.open(File.join(Dir.home, '.shuttl/info'), 'w') do |fi|
+            fi.write base.to_json
+        end
+    end
+
+end
